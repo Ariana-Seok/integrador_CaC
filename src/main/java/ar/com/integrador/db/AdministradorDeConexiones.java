654a0169dbb841019192838c459a8e106b889035
Daniela Fernandez Laura <dfernandezl@fi.uba.ar>
2023-12-17 18:37:40 -0300

    Carga de clases e interfaces para parte Back-end del proyecto

---
+++ src/main/java/ar/com/integrador/db/AdministradorDeConexiones.java
@@ -0,0 +1,33 @@
+package ar.com.integrador.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class AdministradorDeConexiones {
+	// Creamos un metodo estatico (No hay necesidad de instanciar un objeto para usar el metodo)
+	public static Connection getConnection() {
+		String hosts = "localhost";
+		String port = "3306";
+		String password = "";
+		String username = "root";
+		String nombredb = "integrador_cac";
+		
+		// Drive de conexion a la base de datos
+		String driveClassName = "com.mysql.cj.jdbc.Driver";
+		// Aplico manejo de excepciones.
+		Connection connection;
+		try {
+			// com.mysql.cj.jdbc Contruye una clase a partir de un objeto compilado
+			Class.forName(driveClassName);
+			// URL de conexion
+			String url = "jdbc:mysql://"+hosts+":"+port+"/"+nombredb+"?serverTimeZone=UTC&useSSL=false";
+			connection = DriverManager.getConnection(url, username, password);
+			
+		} catch (Exception e){
+			connection = null;
+		}
+		
+		return connection;
+	}
+}
